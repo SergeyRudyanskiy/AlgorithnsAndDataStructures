75f6d84122209f8201f9cce71dbfac8738cc11de
SergeyRudyanskiy <49032709+SergeyRudyanskiy@users.noreply.github.com>
2021-01-31 15:49:30 +0200

    min_max function for counting sort algorithm

--- AlgorithnsAndDataStructures/Sort_Count.hpp
+++ AlgorithnsAndDataStructures/Sort_Count.hpp
@@ -1,10 +1,47 @@
 #pragma once
 #include "Helper_Sort.hpp"
 #include <iterator>
+#include <algorithm>
+#include <tuple>
+
+template<class Iterator, class Comparator>
+std::pair<Iterator, Iterator> minmax_element(Iterator iterator_begin, Iterator iterator_end, Comparator function_comparation) {
+    Iterator iterator_minimum = iterator_begin;
+    Iterator iterator_maximum = iterator_begin;
+
+    if (iterator_begin == iterator_end || ++iterator_begin == iterator_end) { return std::make_pair<Iterator, Iterator>(iterator_minimum, iterator_maximum); }
+    if (function_comparation(*iterator_begin, *iterator_minimum)) { iterator_minimum = iterator_begin; }
+    else {
+        iterator_maximum = iterator_begin;
+    }
+
+    while (++iterator_begin != iterator_end) {
+        Iteartor iterator_temporary = iterator_begin;
+        if (++iterator_begin == iterator_end) {
+            if (function_comparation(*iterator_temporary, *iterator_minimum)) iterator_minimum = iterator_temporary;
+            else if (!(function_comparation(*iterator_temporary, *iterator_maximum))) iterator_maximum = iterator_temporary;
+            break;
+        }
+        else {
+            if (function_comparation(*iterator_begin, *iterator_temporary)) {
+                if (function_comparation(*iterator_begin, *iterator_minimum)) iterator_minimum = iterator_begin;
+                if (!(function_comparation(*iterator_temporary, *iterator_maximum))) iterator_maximum = iterator_temporary;
+            }
+            else {
+                if (function_comparation(*iterator_temporary, *iterator_minimum)) iterator_minimum = iterator_temporary;
+                if (!(function_comparation(*iterator_begin, *iterator_maximum))) iterator_maximum = iterator_begin;
+            }
+        }
+    }
+    return { iterator_minimum, iterator_maximum };
+}
+
 
 template<typename Iterator, typename Comparator>
 void sort_count(Iterator iterator_begin, Iterator iterator_end, Comparator function_comparation) {
-
+    Iterator iterator_minimum;
+    Iterator iterator_maximum;
+    std::tie(iterator_minimum, iterator_maximum) = std::minmax_element(iterator_begin, iterator_end, function_comparation);
 }
 
 template<typename Iterator>
