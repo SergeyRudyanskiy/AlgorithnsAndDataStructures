d441241984e9c438ee3f2cd39f08191099041b7a
SergeyRudyanskiy <49032709+SergeyRudyanskiy@users.noreply.github.com>
2021-02-04 15:26:16 +0200

    somme fixes and less code optimization

--- AlgorithnsAndDataStructures/Sort_Count_Optimized_Map.hpp
+++ AlgorithnsAndDataStructures/Sort_Count_Optimized_Map.hpp
@@ -7,19 +7,15 @@
 
 template<typename Iterator, typename Comparator>
 void sort_count(Iterator iterator_begin, Iterator iterator_end, Comparator function_comparation) {
-    std::map<typename std::iterator_traits<Iterator>::value_type, size_t> map_frequency;
+    using ElementType = typename std::iterator_traits<Iterator>::value_type;
+    std::map<ElementType, size_t> map_frequency;
 
     for (Iterator iterator_curent = iterator_begin; iterator_curent != iterator_end; ++iterator_curent) {
-        if (map_frequency.find(*iterator_curent) == map_frequency.end()) { //checking if elements already exist in map 
-            map_frequency[*iterator_curent] = 1;                           // if no, count of the element is 1
-        }
-        else {
-            map_frequency[*iterator_curent]++;                             // if yes, count of the element is incremented 
-        }
+        map_frequency[*iterator_curent]++;
     }
 
-    typename std::map<typename std::iterator_traits<Iterator>::value_type, size_t>::iterator iterator_curent = map_frequency.begin();
-    for (iterator_curent; iterator_curent != map_frequency.end(); ++iterator_curent) {
+    
+    for (typename std::map<ElementType, size_t>::iterator iterator_curent = map_frequency.begin(); iterator_curent != map_frequency.end(); ++iterator_curent) {
         iterator_begin = std::fill_n(iterator_begin, iterator_curent->second, iterator_curent->first);
     }
 }
