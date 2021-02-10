555fdd5cace715a3fa03efb1d94d129bdc51b7e5
SergeyRudyanskiy <49032709+SergeyRudyanskiy@users.noreply.github.com>
2021-02-10 14:08:50 +0200

    added radix sort for int (32bit positive and negative  integer)

--- AlgorithnsAndDataStructures/AlgorithnsAndDataStructures.cpp
+++ AlgorithnsAndDataStructures/AlgorithnsAndDataStructures.cpp
@@ -3,21 +3,33 @@
 
 #include "Display_Data.hpp"
 #include "Sort_Count_Optimized.hpp"
+#include "Sort_Radix_Binary.hpp"
 
 int main() {
     std::vector<int16_t> vi = { 0, -25, 45, 11, 0, 0, 57, 23, 91, 111, 62, -43 };
     std::cout << "\narray" << std::endl;
-    print_data(vi.begin(), vi.end());
+    //print_data(vi.begin(), vi.end());
 
-    //sort_count(vi.begin(), vi.end(), std::less<std::vector<int16_t>::value_type>{});
-
-    sort_count(vi, std::greater<std::vector<int16_t>::value_type>());
-    std::cout << "\n\nsorted array" << std::endl;
-    print_data(vi);
+    ////sort_count(vi.begin(), vi.end(), std::less<std::vector<int16_t>::value_type>{});
+    //
+    //sort_count(vi, std::greater<std::vector<int16_t>::value_type>());
+    //std::cout << "\n\nsorted array" << std::endl;
+    //print_data(vi);
 
     //std::vector<int16_t>::iterator a, b;
     //std::tie(a, b) = search_element_maximum_minimum(vi.begin(), vi.end());
     //std::cout << *a << " " << *b << std::endl;
+
+    int data[] = { 170, 45, 75, -90, -802, 24, 2, 66 };
+    std::copy(data, data + 8, std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    //lsd_radix_sort(data, data + 8);
+    sort_radix_msd(data, data + 8);
+
+    std::copy(data, data + 8, std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
 
     std::cout << "Hello World!\n";
 }
--- AlgorithnsAndDataStructures/Sort_Radix_Binary.hpp
+++ AlgorithnsAndDataStructures/Sort_Radix_Binary.hpp
@@ -1,1 +1,38 @@
 #pragma once
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <stdint.h>
+
+// Radix sort comparator for 32-bit two's complement integers
+class RadixComparator {
+    const uint8_t bit; // bit position [0..31] to examine
+public:
+    RadixComparator(uint8_t offset) : bit(offset) {} // constructor
+
+    bool operator()(int value) const {// function call operator
+        //if (bit == 31) // sign bit
+        //    return value < 0; // negative int to left partition
+        //else
+        //    return !(value & (1 << bit)); // 0 bit to left partition
+
+        return bit == 31 ? value < 0 : !(value & (1 << bit));
+    }
+};
+
+// Least significant digit radix sort
+void sort_radix_lsd(int* first, int* last) {
+    for (int least_significant_bit = 0; least_significant_bit < 32; ++least_significant_bit) { // least-significant-bit 
+        std::stable_partition(first, last, RadixComparator(least_significant_bit));
+    }
+}
+
+// Most significant digit radix sort (recursive)
+void sort_radix_msd(int* first, int* last, int most_significant_bit = 31) {
+    if (first != last && most_significant_bit >= 0) {
+        int* mid = std::partition(first, last, RadixComparator(most_significant_bit));
+        --most_significant_bit;                           // decrement most-significant-bit
+        sort_radix_msd(first, mid, most_significant_bit); // sort left partition
+        sort_radix_msd(mid, last, most_significant_bit);  // sort right partition
+    }
+}
