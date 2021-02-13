a9ba35971c52053903e7811fe1dd37ea113e3947
SergeyRudyanskiy <49032709+SergeyRudyanskiy@users.noreply.github.com>
2021-02-13 15:41:50 +0200

    added implementation of binary radix sort(both lsd & msb)

--- AlgorithnsAndDataStructures/Sort_Radix_Binary.hpp
+++ AlgorithnsAndDataStructures/Sort_Radix_Binary.hpp
@@ -1,38 +1,76 @@
 #pragma once
 #include <algorithm>
-#include <iostream>
 #include <iterator>
 #include <stdint.h>
+#include <type_traits>
 
-// Radix sort comparator for 32-bit two's complement integers
-class RadixComparator {
-    const uint8_t bit; // bit position [0..31] to examine
+//auto lambda_comparator_less = [significant_bit](int x) { return (significant_bit == (bit_size(x) - 1u)) ? x < 0 : !(x & (1 << significant_bit)); }
+const uint8_t BITS_PER_BYTE = 8u;
+
+template<typename ElementType>
+uint16_t bit_size(ElementType value ) {
+    return sizeof(value) * BITS_PER_BYTE;
+}
+
+template<typename ElementType>
+class ComparatorRadix {
 public:
-    RadixComparator(uint8_t offset) : bit(offset) {} // constructor
+    ComparatorRadix() = default;
+    ComparatorRadix(ElementType offset_bit) : significant_bit(offset_bit) {};
 
-    bool operator()(int value) const {// function call operator
-        //if (bit == 31) // sign bit
-        //    return value < 0; // negative int to left partition
-        //else
-        //    return !(value & (1 << bit)); // 0 bit to left partition
+    void set_significant_bit(uint16_t offset_bit) { significant_bit = offset_bit; };
+protected:
+    uint16_t significant_bit;
+};
 
-        return bit == 31 ? value < 0 : !(value & (1 << bit));
-    }
+template<typename ElementType>
+class ComparatorRadixLess : public ComparatorRadix<ElementType> {
+public:
+    ComparatorRadixLess() = default;
+    ComparatorRadixLess(uint16_t offset_bit) : ComparatorRadix<ElementType>(offset_bit) {};
+
+    bool operator()(ElementType value) const {                                                                                // sign bit
+        return (this->significant_bit == (bit_size(value) - 1u)) ? value < 0 : !(value & (1 << this->significant_bit));       // negative int to left partition
+    }                                                                                                                         // 0 bit to left partition
+};
+
+template<typename ElementType>
+class ComparatorRadixGreater : public ComparatorRadix<ElementType> {
+public:
+    ComparatorRadixGreater() = default;
+    ComparatorRadixGreater(uint16_t offset_bit) : ComparatorRadix<ElementType>(offset_bit) {};
+
+    bool operator()(ElementType value) const {                                                                                // sign bit
+        return (this->significant_bit == (bit_size(*value) - 1u)) ? *value > 0 : (*value & (1 << this->significant_bit));        // positive int to left partition
+    }                                                                                                                         // 0 bit to right partition
 };
 
 // Least significant digit radix sort
-void sort_radix_lsd(int* first, int* last) {
-    for (int least_significant_bit = 0; least_significant_bit < 32; ++least_significant_bit) { // least-significant-bit 
-        std::stable_partition(first, last, RadixComparator(least_significant_bit));
+template <typename Iterator, typename Comparator = typename ComparatorRadixLess<typename std::iterator_traits<Iterator>::value_type>>
+void sort_radix_lsd(Iterator iterator_begin, Iterator Iterator_end, Comparator class_comparator = Comparator()) {
+    const uint16_t count_bit = sizeof(typename std::iterator_traits<Iterator>::value_type) * BITS_PER_BYTE;
+    for (uint16_t curent_least_significant_bit = 0; curent_least_significant_bit < count_bit; ++curent_least_significant_bit) { //least-significant-bit 
+        class_comparator.set_significant_bit(curent_least_significant_bit);
+        std::stable_partition(iterator_begin, Iterator_end, class_comparator);
     }
 }
-
 // Most significant digit radix sort (recursive)
-void sort_radix_msd(int* first, int* last, int most_significant_bit = 31) {
-    if (first != last && most_significant_bit >= 0) {
-        int* mid = std::partition(first, last, RadixComparator(most_significant_bit));
-        --most_significant_bit;                           // decrement most-significant-bit
-        sort_radix_msd(first, mid, most_significant_bit); // sort left partition
-        sort_radix_msd(mid, last, most_significant_bit);  // sort right partition
+//void sort_radix_msd1(int* iterator_begin, int* iterator_end, int most_significant_bit = sizeof(int) * BITS_PER_BYTE - 1u, ComparatorRadix<int> *cmp = new ComparatorRadixLess<int>(31)) {
+//    if (iterator_begin != iterator_end && most_significant_bit >= 0) {
+//        int* mid = std::partition(iterator_begin, iterator_end, ComparatorRadixLess<int>(most_significant_bit));
+//        --most_significant_bit;                           // decrement most-significant-bit
+//        sort_radix_msd1(iterator_begin, mid, most_significant_bit); // sort left partition
+//        sort_radix_msd1(mid, iterator_end, most_significant_bit);  // sort right partition
+//    }
+//}
+
+template <typename Iterator, typename Comparator = ComparatorRadixLess<typename std::iterator_traits<Iterator>::value_type>>
+void sort_radix_msd(Iterator iterator_begin, Iterator iterator_end, Comparator class_comparator = Comparator(), uint8_t curent_most_significant_bit = sizeof(typename std::iterator_traits<Iterator>::value_type) * BITS_PER_BYTE - 1u) {
+    class_comparator.set_significant_bit(curent_most_significant_bit);
+    if (iterator_begin != iterator_end && curent_most_significant_bit > 0) {
+        Iterator iterator_midle = std::stable_partition(iterator_begin, iterator_end, class_comparator);
+        --curent_most_significant_bit;                                                                 // decrement most-significant-bit
+        sort_radix_msd(iterator_begin, iterator_midle, class_comparator, curent_most_significant_bit); // sort left partition
+        sort_radix_msd(iterator_midle, iterator_end, class_comparator, curent_most_significant_bit);   // sort right partition
     }
 }
