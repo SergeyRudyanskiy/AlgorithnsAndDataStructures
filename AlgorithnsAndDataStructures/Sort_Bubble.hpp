b32a1fd27be977b40fa2a8b383445176093a8969
SergeyRudyanskiy <49032709+SergeyRudyanskiy@users.noreply.github.com>
2021-02-16 13:25:17 +0200

    Final Version, Some bug fixes, codestyle

--- AlgorithnsAndDataStructures/Sort_Bubble.hpp
+++ AlgorithnsAndDataStructures/Sort_Bubble.hpp
@@ -1,9 +1,8 @@
 #pragma once
-#include "Helper_Sort.hpp"
 #include <iterator>
 
-template<typename Iterator, typename Comparator>
-void inline sort_bubble(Iterator iterator_begin, Iterator iterator_end, Comparator function_comparation) {
+template<typename Iterator, typename Comparator = typename std::less<typename std::iterator_traits<Iterator>::value_type>>
+void inline sort_bubble(Iterator iterator_begin, Iterator iterator_end, Comparator function_comparation = Comparator()) {
 	for (bool flag_swap_event = true; flag_swap_event && iterator_begin != iterator_end; ++iterator_begin) {
 		flag_swap_event = false;
 		for (Iterator iterator_current = std::next(iterator_begin); iterator_current != iterator_end; std::advance(iterator_current, 1)) {
@@ -15,8 +14,7 @@ void inline sort_bubble(Iterator iterator_begin, Iterator iterator_end, Comparat
 	}
 }
 
-template <typename Iterator>
-void sort_bubble(Iterator iterator_begin, Iterator iterator_end) {
-	sort_bubble(iterator_begin, iterator_end, std::less<typename std::iterator_traits<Iterator>::value_type>{});
+template<typename Container, typename Comparator = typename std::less<typename Container::value_type>>
+void inline sort_bubble(Container& container, Comparator function_comparation = Comparator()) {
+	sort_bubble(std::begin(container), std::end(container), function_comparation);
 }
-
