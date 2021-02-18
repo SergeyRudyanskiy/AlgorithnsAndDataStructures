c9abfde5d23019163f790440146d14ca206587c7
SergeyRudyanskiy <49032709+SergeyRudyanskiy@users.noreply.github.com>
2021-02-18 16:29:06 +0200

    Some fixes

--- AlgorithnsAndDataStructures/Search_Element_Binary_Itarative.hpp
+++ AlgorithnsAndDataStructures/Search_Element_Binary_Itarative.hpp
@@ -1,14 +1,14 @@
 #pragma once
 template<typename Iterator, typename ElementType>
-Iterator search_binary_iterative(Iterator iterator_begin, Iterator iterator_end, const ElementType value_key) {
+Iterator search_binary_iterative(Iterator iterator_begin, Iterator iterator_end, const ElementType key_value) {
 	const Iterator iterator_not_found = iterator_end;
 	Iterator iterator_midle;
 
 	while (iterator_begin <= iterator_end) {
 		iterator_midle = iterator_begin + (iterator_end - iterator_begin) / 2;
 
-		if		(*iterator_midle > value_key) { iterator_end   = --iterator_midle; }
-		else if (*iterator_midle < value_key) { iterator_begin = ++iterator_midle; }
+		if		(*iterator_midle > key_value) { iterator_end   = --iterator_midle; }
+		else if (*iterator_midle < key_value) { iterator_begin = ++iterator_midle; }
 		else								  { return iterator_midle;			   }
 	}
 
--- AlgorithnsAndDataStructures/Search_Element_Binary_Recursive.hpp
+++ AlgorithnsAndDataStructures/Search_Element_Binary_Recursive.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
 template<typename Iterator, typename ElementType>
-Iterator search_binary_recursive(Iterator iterator_begin, Iterator iterator_end, const ElementType value_key) {
+Iterator search_binary_recursive(Iterator iterator_begin, Iterator iterator_end, const ElementType key_value) {
 	static const Iterator iterator_not_found = iterator_end;
 	Iterator iterator_midle;
 
 	if (iterator_begin <= iterator_end) {
 		iterator_midle = iterator_begin + (iterator_end - iterator_begin) / 2;
 
-		if		(*iterator_midle > value_key) { return search_binary_recursive(iterator_begin, --iterator_midle, value_key); }
-		else if (*iterator_midle < value_key) { return search_binary_recursive(++iterator_midle, iterator_end, value_key);	 }
+		if		(*iterator_midle > key_value) { return search_binary_recursive(iterator_begin, --iterator_midle, key_value); }
+		else if (*iterator_midle < key_value) { return search_binary_recursive(++iterator_midle, iterator_end, key_value);	 }
 		else								  { return iterator_midle;														 }
 	}
 
