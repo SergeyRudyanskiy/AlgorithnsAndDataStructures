67d4ac7fa3c5903148115eb8096bacea974f568a
SergeyRudyanskiy <49032709+SergeyRudyanskiy@users.noreply.github.com>
2021-02-21 00:02:41 +0200

    Rename(mistake)

--- AlgorithnsAndDataStructures/Search_Element_Binary_Itarative.hpp
+++ AlgorithnsAndDataStructures/Search_Element_Binary_Itarative.hpp
@@ -1,15 +1,14 @@
 #pragma once
 template<typename Iterator, typename ElementType>
-Iterator search_binary_iterative(Iterator iterator_begin, Iterator iterator_end, const ElementType key_value) {
+Iterator search_element_binary_iterative(Iterator iterator_begin, Iterator iterator_end, const ElementType key_value) {
 	const Iterator iterator_not_found = iterator_end;
 	Iterator iterator_midle;
 
-	while (iterator_begin <= iterator_end) {
+	while (iterator_begin < iterator_end) {
 		iterator_midle = iterator_begin + (iterator_end - iterator_begin) / 2;
 
-		if		(*iterator_midle > key_value) { iterator_end   = --iterator_midle; }
-		else if (*iterator_midle < key_value) { iterator_begin = ++iterator_midle; }
-		else								  { return iterator_midle;			   }
+		if (*iterator_midle == key_value) { return iterator_midle; }
+		(*iterator_midle > key_value) ? iterator_end = iterator_midle : iterator_begin = ++iterator_midle;
 	}
 
 	return iterator_not_found;
