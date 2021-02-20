dafd7bf61175bfb1ece1985027640f8cad9e14f0
SergeyRudyanskiy <49032709+SergeyRudyanskiy@users.noreply.github.com>
2021-02-21 00:38:54 +0200

    tested

--- AlgorithnsAndDataStructures/Search_Element_Binary_Recursive.hpp
+++ AlgorithnsAndDataStructures/Search_Element_Binary_Recursive.hpp
@@ -1,16 +1,16 @@
 #pragma once
 
 template<typename Iterator, typename ElementType>
-Iterator search_binary_recursive(Iterator iterator_begin, Iterator iterator_end, const ElementType key_value) {
+Iterator search_element_binary_recursive(Iterator iterator_begin, Iterator iterator_end, const ElementType key_value) {
 	static const Iterator iterator_not_found = iterator_end;
 	Iterator iterator_midle;
 
-	if (iterator_begin <= iterator_end) {
+	if (iterator_begin < iterator_end) {
 		iterator_midle = iterator_begin + (iterator_end - iterator_begin) / 2;
 
-		if		(*iterator_midle > key_value) { return search_binary_recursive(iterator_begin, --iterator_midle, key_value); }
-		else if (*iterator_midle < key_value) { return search_binary_recursive(++iterator_midle, iterator_end, key_value);	 }
-		else								  { return iterator_midle;														 }
+		if		(*iterator_midle > key_value) { return search_element_binary_recursive(iterator_begin, iterator_midle, key_value); }
+		else if (*iterator_midle < key_value) { return search_element_binary_recursive(++iterator_midle, iterator_end, key_value); }
+		else								  { return iterator_midle;															   }
 	}
 
 	return iterator_not_found;
