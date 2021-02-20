16833e32e4b55fd4db9a188f7737863e2920c20c
SergeyRudyanskiy <49032709+SergeyRudyanskiy@users.noreply.github.com>
2021-02-21 00:04:05 +0200

    implementation of calculating digit count

--- AlgorithnsAndDataStructures/Function_Calculate_Digit_Count.hpp
+++ AlgorithnsAndDataStructures/Function_Calculate_Digit_Count.hpp
@@ -5,7 +5,7 @@
 
 template<typename ElementType>
 uint8_t calculate_digit_count_version_0(ElementType value) {
-    uint8_t digit_count = 0;
+    uint8_t digit_count = 0u;
     while (value / 10) { ++digit_count; }
 
     return digit_count == 0u ? 1u : digit_count;
@@ -24,9 +24,6 @@ uint8_t calculate_digit_count_version_2(ElementType value) {
 template<typename ElementType>
 uint8_t calculate_digit_count_version_3(ElementType value) {
     std::string string_representation_of_value = std::to_string(value);
-    return static_cast<uint8_t>(string_representation_of_value.size());
-}
 
-template<typename ElementType>
-uint8_t calculate_digit_count_version_4(ElementType value) {
+    return static_cast<uint8_t>(string_representation_of_value.size());
 }
