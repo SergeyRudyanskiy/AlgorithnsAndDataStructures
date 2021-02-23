fe3e437348238395e16c32c453d9336ba3966ba1
SergeyRudyanskiy <49032709+SergeyRudyanskiy@users.noreply.github.com>
2021-02-23 17:16:30 +0200

    fixed code style

--- AlgorithnsAndDataStructures/Function_Odd_Even.hpp
+++ AlgorithnsAndDataStructures/Function_Odd_Even.hpp
@@ -2,20 +2,20 @@
 
 template<typename ElementType>
 inline bool is_even_version_0(ElementType& value) {
-	return ((value % 2) == 0) ? true : false;
+	return value % 2 == 0;
 }
 
 template<typename ElementType>
 inline bool is_even_version_1(ElementType& value) {
-	return ((value & 1) == 0) ? true : false;
+	return value & 1 == 0;
 }
 
 template<typename ElementType>
 inline bool is_odd_version_0(ElementType& value) {
-	return ((value % 2) == 0) ? false : true;
+	return !(value % 2 == 0);
 }
 
 template<typename ElementType>
 inline bool is_odd_version_1(ElementType& value) {
-	return ((value & 1) == 0) ? false : true;
+	return !(value & 1 == 0);
 }
